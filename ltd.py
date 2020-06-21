4686b10134b6e84263694ed0217fc782c22d3ce0
pardu1904 <67218432+pardu1904@users.noreply.github.com>
2020-06-21 12:41:46 +0530

    the code deals with lists,tuple,dictionary in python

---
+++ ltd.py
@@ -0,0 +1,21 @@
+#objective 1
+
+nums=[1,2,3]
+nums.append("4")
+print(nums)
+
+#objective 2
+
+a=(1,2,3) #assigning the tuple a
+print(a[0]) #accessing the first element in tuple a
+print(a[1]) #accessing the second element in tuple a
+print(a[2]) #accessing the third element in tuple a
+print(a[-1]) #accessing the first element from last in tuple a
+print(a[-2]) #accessing the second element from last in tuple a 
+print(a[-3])
+
+#objective 3
+
+c={1:'hello',2:'world',3:'program'}
+del c[1]
+print(c)
